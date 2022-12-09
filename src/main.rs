3dda89866ca3eb9459de514191fcf1ddbbafc594
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-12-09 14:25:00 -0600

    Completed Day 01 Parts 1 & 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#![allow(non_snake_case)]
+
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let input = calarie_inventory();
+
+    let mut inv: Vec<i32> = Vec::new();
+    let mut t: i32 = 0;
+
+    for line in input.lines() {
+        let l = line.unwrap();
+
+        if l == "" {
+            inv.push(t);
+            t = 0;
+        } else {
+            let n: i32 = l.parse().unwrap();
+            t = t + n;
+        }
+    }
+    inv.sort();
+    let inv_len = inv.len();
+    let max_inv = inv.get(inv_len - 1).unwrap();
+    let max_three = inv.get(inv_len - 1).unwrap() + inv.get(inv_len - 2).unwrap() + inv.get(inv_len - 3).unwrap();
+
+    println!("Day 01 Part 01: {max_inv}");
+    println!("Day 01 Part 02: {max_three}");
+
+}
+
+fn calarie_input() -> std::fs::File {
+    match File::open("CalarieInput") {
+        Ok(f) => { f },
+        Err(e) => { panic!("Error reading CalarieInput: {e}"); }
+    }
+}
+
+fn calarie_inventory() -> std::io::BufReader<std::fs::File> {
+    BufReader::new(calarie_input())
 }
